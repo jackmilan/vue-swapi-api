20332ec7af8a35b1335d618343825d8d4667d5cf
jackmilan <jackmilan97@gmail.com>
2022-06-16 18:18:54 +0100

    installed and set upped terser-webpack-plugin

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,6 @@
 const { defineConfig } = require('@vue/cli-service');
+const TerserPlugin = require('terser-webpack-plugin')
+
 module.exports = defineConfig({
   transpileDependencies: ['quasar'],
 
@@ -12,4 +14,29 @@ module.exports = defineConfig({
       rtlSupport: false,
     },
   },
+
+  configureWebpack: config => {
+    config.optimization = {
+      minimize: true,
+      minimizer: [
+        new TerserPlugin({
+          terserOptions: {
+            ecma: undefined,
+            warnings: false,
+            parse: {},
+            compress: { drop_console: true },
+            mangle: true,
+            module: false,
+            output: { comments: false },
+            toplevel: false,
+            nameCache: null,
+            ie8: false,
+            keep_classnames: undefined,
+            keep_fnames: false,
+            safari10: false,
+          },
+        }),
+      ],
+    }
+  }
 });
